b3c943743c92e7027cdab6303dab2be5c7c2f3e2
Tanmayee Kalluri <tanmayee7@gmail.com>
2020-01-31 23:33:19 -0500

    Initial push

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {Home} from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+     
+      <Link to="/">Home</Link>
+      <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </Router>
+     
+        </div>
+    );
+}
+
+export default App;
