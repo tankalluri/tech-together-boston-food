4badd69e4d6b51cde9bd7b25025ba5bf2d332184
Tanmayee Kalluri <tanmayee7@gmail.com>
2020-02-01 00:09:58 -0500

    Complete card

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-     
       <Link to="/">Home</Link>
       <Switch>
           <Route exact path="/">
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,42 @@
 import React, {Component} from 'react';
-export class Home extends Component{
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import '../src/Home.css'
+
+  export class Home extends Component {
     render(){
-        return <div>Hehe</div>
+    return (
+      <Card className="card">
+        <CardActionArea>
+          <CardMedia
+            className="media"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Grapes
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              These are grapes. Please eat them.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+           <FavoriteIcon/>
+          </Button>
+          <Button size="small" color="primary">
+            Click for more details
+          </Button>
+        </CardActions>
+      </Card>
+    );
     }
-}
+  }
